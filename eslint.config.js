47641dda6add7d1d25ba7ff976051237b330364b
Dernoa <dernoa@zohomail.com>
2025-06-10 22:17:14 +0300

    added ESLint rules
    Added ESLint and checked workability

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,22 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: {
+      'react-native': require('eslint-plugin-react-native'),
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react-native/no-inline-styles': 'warn',
+      'react-native/no-color-literals': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-unused-vars': 'off',
+    },
   },
 ]);
